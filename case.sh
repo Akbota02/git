8c735d1b5511155125800da08d6ebfb787cb1593
Akbota Dauylbay <d.akbota36@gmail.com>
2021-12-05 22:09:15 +0600

    commit_3

--- case.sh
+++ case.sh
@@ -3,5 +3,9 @@ echo "Введите марку телефона"
 read brand 
 case $brand in 
 	samsung)
-		echo "Скидка на телефоны $brand - 30%"
+		echo "Скидка на телефоны $brand - 30%";;
+	oppo)
+		echo "Скидка на телефоны $brand - 10%";;
+	huawei) 
+		echo "Скидка на телефоны $brand - 20%"
 esac
